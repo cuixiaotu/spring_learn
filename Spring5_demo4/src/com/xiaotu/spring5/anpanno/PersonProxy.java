915424090625f658b30eab4d14e882b1b305dc80
theodore <cuixin1974@stnts.com>
2022-02-11 16:14:52 +0800

    Spring AOP

---
+++ Spring5_demo4/src/com/xiaotu/spring5/anpanno/PersonProxy.java
@@ -0,0 +1,21 @@
+package com.xiaotu.spring5.anpanno;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Aspect//表示此类是增强类
+@Order(1)
+public class PersonProxy {
+
+    //前置通知
+    //@Before注解表示作为前置通知
+    @Before(value = "execution(* com.xiaotu.spring5.aopanno.User.add(..))")
+    public void before() {
+        System.out.println("PersonProxy.before....");
+    }
+
+}
